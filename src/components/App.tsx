a12a3fa561ed02abb32f83f6be5c3dfe64224ac4
Grant Warman <grant.warman@gmail.com>
2025-01-04 11:27:27 -0700

    udate to react router

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,14 +1,14 @@
 import React from 'react';
-import { HashRouter, Route, Switch } from 'react-router-dom';
+import { HashRouter, Route, Routes } from 'react-router';
 import HomePage from './routes/home/HomePage';
 import settings from '../../settings';
 
 export default function App() {
   return (
-    <HashRouter basename={settings.repoPath}>
-      <Switch>
-        <Route exact path="/" component={HomePage} />
-      </Switch>
+    <HashRouter>
+      <Routes>
+        <Route path="/" element={<HomePage />} />
+      </Routes>
     </HashRouter>
   );
 }
